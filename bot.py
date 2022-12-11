eb54315219594a5f47d9e2a05d91879b07cd0930
Konstantin Oreshkin <oreshkin007@mail.ru>
2022-12-11 22:12:12 +0300

    change variable mistake

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ def write_msg(user_id, message, attachment):
     vk_session.method("messages.send", {"user_id": user_id, "message": message, "attachment": attachment,  "random_id": get_random_id()})
 
 offset = 0
-gоt_user = []
+got_user = []
 shown_couple = []
 
 def get_user():
